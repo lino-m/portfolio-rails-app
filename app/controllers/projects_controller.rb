354f9606d09fd0c04acaed4a65205d229b6ada7b
Lino Maurer <lino@q-b.de>
2020-11-15 15:56:22 +0100

    created functionality and views(form partial) for edit and update actions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,15 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: %i[show edit]
+
   def index
     @projects = Project.all
   end
 
   def show; end
 
-  def new; end
+  def new
+    @project = Project.new
+  end
 
   def create; end
 
@@ -14,4 +18,14 @@ class ProjectsController < ApplicationController
   def update; end
 
   def destroy; end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :category, :description, :body, :repo_link)
+  end
 end
